a275f4ce00caab0b67fe90da46647004fc92c021
Tripp Hammond <tripphamm@gmail.com>
2020-01-03 13:53:29 -0500

    Internal: Debugging statement

--- bin/release.js
+++ bin/release.js
@@ -7,6 +7,8 @@ const env = localEnv.read();
 
 localEnv.validate(env, ['GITHUB_TOKEN', 'NPM_TOKEN']);
 
+console.log(env['GITHUB_TOKEN']);
+
 // --ci false skips the only-ci check
 // this allows us to execute the release locally if we want
 const result = spawn.sync('semantic-release', ['--ci', 'false'], {
