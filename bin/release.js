0cccb623f6d659c7df65f382c9b9a6ca9d83cde5
Tripp Hammond <tripphamm@gmail.com>
2020-01-03 14:11:02 -0500

    Fix: Remove console log in release script

--- bin/release.js
+++ bin/release.js
@@ -7,11 +7,9 @@ const env = localEnv.read();
 
 localEnv.validate(env, ['GITHUB_TOKEN', 'NPM_TOKEN']);
 
-console.log(env['GITHUB_TOKEN']);
-
 // --ci false skips the only-ci check
 // this allows us to execute the release locally if we want
-const result = spawn.sync('semantic-release', ['--ci', 'false'], {
+const result = spawn.sync('semantic-release', ['--allow-same-version', '--ci', 'false'], {
   env,
   stdio: 'inherit',
 });
