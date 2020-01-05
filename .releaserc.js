84e1c05bf77965c1918eec0e96560816bcb631d7
Tripp Hammond <tripphamm@gmail.com>
2020-01-05 14:19:12 -0500

    Fix: Remove lerna

--- .releaserc.js
+++ .releaserc.js
@@ -1,9 +1,1 @@
-module.exports = {
-  branch: 'master',
-  preset: 'eslint',
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
-  ],
-};
+module.exports = require('@tripphamm/trippkit/configs/semantic-release');
--- scripts/release.js
+++ scripts/release.js
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
 
 const spawn = require('cross-spawn');
-const fs = require('fs');
 const localEnv = require('@tripphamm/trippkit/utils/env');
 
 const env = localEnv.read();
 
 localEnv.validate(env, ['GITHUB_TOKEN', 'NPM_TOKEN']);
-
-// some tools use GH_TOKEN rather than GITHUB_TOKEN
-env['GH_TOKEN'] = env['GITHUB_TOKEN'];
 
 const spawnOpts = {
   env,
@@ -17,31 +13,12 @@ const spawnOpts = {
   stdio: 'inherit',
 };
 
-if (env['CI'] === 'true') {
-  spawn.sync('git', ['config', 'user.name', 'CI User'], spawnOpts);
-  spawn.sync('git', ['config', 'user.email', 'robot@ci.com'], spawnOpts);
-
-  // we need a .npmrc file which references the NPM_TOKEN env var
-  // note that this isn't actually embedding the token itself into the file, just the literal string "${NPM_TOKEN}"
-  // we _could_ just commit this file to the repo, but that would require devs to have NPM_TOKEN defined on their system
-  // and we don't want to require that
-  try {
-    fs.writeFileSync('./.npmrc', '//registry.npmjs.org/:_authToken=${NPM_TOKEN}', {
-      encoding: 'utf8',
-    });
-  } catch (error) {
-    console.error(error);
-
-    process.exit(1);
-  }
-}
-
-const lernaPublishResult = spawn.sync(
-  'lerna',
-  ['publish', '--yes', '--conventional-commits', '--changelog-preset', 'eslint', '--verbose'],
+const result = spawn.sync(
+  'semantic-release',
+  ['-e', 'semantic-release-monorepo', '--ci', 'false'],
   spawnOpts,
 );
 
-if (lernaPublishResult.status !== 0) {
-  process.exit(lernaPublishResult.status);
+if (result.status !== 0) {
+  process.exit(result.status);
 }
