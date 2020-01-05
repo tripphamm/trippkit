7e199a3c3022925a974bc2ec5f542d79e5fe1e27
Tripp Hammond <tripphamm@gmail.com>
2020-01-05 13:23:26 -0500

    Fix: Try to simplify with lerna publish conventional commits

--- scripts/release.js
+++ scripts/release.js
@@ -8,6 +8,9 @@ const env = localEnv.read();
 
 localEnv.validate(env, ['GITHUB_TOKEN', 'NPM_TOKEN']);
 
+// some tools use GH_TOKEN rather than GITHUB_TOKEN
+env['GH_TOKEN'] = env['GITHUB_TOKEN'];
+
 const spawnOpts = {
   env,
   encoding: 'utf8',
@@ -17,41 +20,41 @@ const spawnOpts = {
 if (env['CI'] === 'true') {
   console.log('CI Environment detected');
 
-  try {
-    // in CI we need to embed our github token into the origin url so that we can push a commit
-    const { repository } = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf8' }));
+  // try {
+  //   // in CI we need to embed our github token into the origin url so that we can push a commit
+  //   const { repository } = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf8' }));
 
-    // we need to inject the auth info after the prefix and before the rest
+  //   // we need to inject the auth info after the prefix and before the rest
 
-    const prefix = 'https://';
+  //   const prefix = 'https://';
 
-    if (!repository.startsWith(prefix)) {
-      console.error(`Expected repository to start with ${prefix}. Got,`, repository);
-      process.exit(1);
-    }
+  //   if (!repository.startsWith(prefix)) {
+  //     console.error(`Expected repository to start with ${prefix}. Got,`, repository);
+  //     process.exit(1);
+  //   }
 
-    const authenticatedGitOrigin = [
-      repository.slice(0, prefix.length),
-      `tripphamm:${env['GITHUB_TOKEN']}@`,
-      repository.slice(prefix.length),
-    ].join('');
+  //   const authenticatedGitOrigin = [
+  //     repository.slice(0, prefix.length),
+  //     `tripphamm:${env['GITHUB_TOKEN']}@`,
+  //     repository.slice(prefix.length),
+  //   ].join('');
 
-    const gitSetOriginResult = spawn.sync(
-      'git',
-      ['remote', 'set-url', 'origin', authenticatedGitOrigin],
-      spawnOpts,
-    );
+  //   const gitSetOriginResult = spawn.sync(
+  //     'git',
+  //     ['remote', 'set-url', 'origin', authenticatedGitOrigin],
+  //     spawnOpts,
+  //   );
 
-    if (gitSetOriginResult.status !== 0) {
-      process.exit(gitSetOriginResult.status);
-    }
+  //   if (gitSetOriginResult.status !== 0) {
+  //     process.exit(gitSetOriginResult.status);
+  //   }
 
-    console.log('Set origin to: ', authenticatedGitOrigin);
-  } catch (error) {
-    console.error(error);
+  //   console.log('Set origin to: ', authenticatedGitOrigin);
+  // } catch (error) {
+  //   console.error(error);
 
-    process.exit(1);
-  }
+  //   process.exit(1);
+  // }
 
   // we need a .npmrc file which references the NPM_TOKEN env var
   // note that this isn't actually embedding the token itself into the file, just the literal string "${NPM_TOKEN}"
@@ -72,68 +75,66 @@ if (env['CI'] === 'true') {
 
 // // --ci false skips the only-ci check
 // // this allows us to execute the release locally if we want
-const semanticReleaseResult = spawn.sync('semantic-release', ['--ci', 'false'], spawnOpts);
+// const semanticReleaseResult = spawn.sync('semantic-release', ['--ci', 'false'], spawnOpts);
 
-if (semanticReleaseResult.status !== 0) {
-  process.exit(semanticReleaseResult.status);
-}
+// if (semanticReleaseResult.status !== 0) {
+//   process.exit(semanticReleaseResult.status);
+// }
 
-console.log('Finished semantic release');
+// console.log('Finished semantic release');
 
 // semantic-release should have tagged the latest commit with the new version
 // we'll use git to grab that tag in order to figure out the proper version to use
-const gitVersionResult = spawn.sync('git', ['describe', '--tags'], {
-  ...spawnOpts,
-  // change stdio to pipe so that we can get access to the output
-  stdio: 'pipe',
-});
+// const gitVersionResult = spawn.sync('git', ['describe', '--tags'], {
+//   ...spawnOpts,
+//   // change stdio to pipe so that we can get access to the output
+//   stdio: 'pipe',
+// });
 
-if (gitVersionResult.status !== 0) {
-  process.exit(gitVersionResult.status);
-}
+// if (gitVersionResult.status !== 0) {
+//   process.exit(gitVersionResult.status);
+// }
 
-const version = gitVersionResult.stdout.trim();
+// const version = gitVersionResult.stdout.trim();
 
-if (/^v\d+\.\d+\.\d+$/.test(version) === false) {
-  console.error('Unexpected version', version);
-  process.exit(1);
-}
+// if (/^v\d+\.\d+\.\d+$/.test(version) === false) {
+//   console.error('Unexpected version', version);
+//   process.exit(1);
+// }
 
-// remove the "v"
-const versionNumber = version.slice(1);
+// // remove the "v"
+// const versionNumber = version.slice(1);
 
-console.log('Found tag for version', version);
+// console.log('Found tag for version', versionNumber);
 
-const lernaVersionResult = spawn.sync(
-  'lerna',
-  [
-    'version',
-    versionNumber,
-    '--yes',
-    '--message',
-    'Chore: Release',
-    '--tag-version-prefix',
-    'lerna-v',
-  ],
-  spawnOpts,
-);
+// const lernaVersionResult = spawn.sync(
+//   'lerna',
+//   [
+//     'version',
+//     versionNumber,
+//     '--yes',
+//     '--message',
+//     'Chore: Release',
+//     '--tag-version-prefix',
+//     'lerna-v',
+//   ],
+//   spawnOpts,
+// );
 
-if (lernaVersionResult.status !== 0) {
-  process.exit(lernaVersionResult.status);
-}
+// if (lernaVersionResult.status !== 0) {
+//   process.exit(lernaVersionResult.status);
+// }
 
-console.log('lerna version updated each package');
+// console.log('lerna version updated each package');
 
-spawn.sync('git', ['status'], spawnOpts);
+// spawn.sync('git', ['status'], spawnOpts);
 
 const lernaPublishResult = spawn.sync(
   'lerna',
-  ['publish', 'from-package', '--yes', '--message', `Chore: Release ${version} [skip ci]`],
+  ['publish', '--yes', '--conventional-commits', '--changelog-preset', 'eslint'],
   spawnOpts,
 );
 
 if (lernaPublishResult.status !== 0) {
   process.exit(lernaPublishResult.status);
 }
-
-console.log('Done');
