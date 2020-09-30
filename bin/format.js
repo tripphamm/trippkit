e47e9740a00591bd182fb029cff2e2cab48b2dc0
EJ Hammond <ejhammond@hey.com>
2020-09-30 10:41:22 -0400

    Fix: Format json and mdx files with jskit-format

--- bin/format.js
+++ bin/format.js
@@ -4,7 +4,7 @@ const spawn = require('cross-spawn');
 
 const result = spawn.sync(
   'prettier',
-  ['**/*.+(js|jsx|ts|tsx)', '--write', '--ignore-path', '.gitignore'],
+  ['**/*.+(js|jsx|ts|tsx|json|mdx)', '--write', '--ignore-path', '.gitignore'],
   {
     stdio: 'inherit',
   },
